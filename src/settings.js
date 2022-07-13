e8970a435ba702e583788a1bb1b4386e33dcb77c
cptbtptp-wb <3020777931@qq.com>
2022-07-13 11:52:43 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '人力资源管理后台',
+
+  /** 设置appMain右侧主体上面的header是不是固定定位
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /** 设置侧边栏上面logo是否显示
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true // 显示logo
+}
